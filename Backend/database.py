03d43e582f62229e2c2cf86cfe310ba2b67e65db
igor serkov <igor.serkov1005@gmail.com>
2023-05-22 19:22:11 +0300

    Add upload file func

--- Backend/database.py
+++ Backend/database.py
@@ -1,6 +1,4 @@
-from firebase_admin import initialize_app
-from firebase_admin import credentials
-from firebase_admin import db
+from firebase_admin import initialize_app, credentials, db, storage
 
 import Discord.Setup.config as config
 
@@ -8,12 +6,14 @@ import Discord.Setup.config as config
 class Base:
     def __init__(self, name):
         self.url = config.database_url
+        self.storage_url = config.database_storage_url
         self.cert = config.database_cert
         self.name = name
+        self.app = None
         self._connect()
 
     def _connect(self):
-        initialize_app(credentials.Certificate(self.cert), {'databaseURL': self.url}, name=self.name)
+        initialize_app(credentials.Certificate(self.cert), {'databaseURL': self.url, 'storageBucket': self.storage_url}, name=self.name)
 
     @staticmethod
     def get_data(ref_path: str):
@@ -29,3 +29,9 @@ class Base:
     def delete_data(ref_path: str):
         ref = db.reference(ref_path)
         ref.delete()
+
+    @staticmethod
+    def upload_file(blob_name: str, data: bytes):
+        bucket = storage.bucket()
+        blob = bucket.blob(blob_name)
+        blob.upload_from_string(data=data)
